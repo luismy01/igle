b36aa02e3062eaea59fb43464d14c7812b1e4c0f
Luis Mejia <luismy01@gmail.com>
2014-08-17 05:17:05 -0500

    Se aplica Class Based View en la app de asistencia

--- asistencia/urls.py
+++ asistencia/urls.py
@@ -2,22 +2,36 @@ from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 
-from views import AsistenciaView
+from views import InicioView, CrearAsistenciaView, EditarAsistenciaView, BorrarAsistenciaView, ListarAsistenciaView, AjaxAsistenciaView
 
 urlpatterns = patterns('asistencia.views',
     
-	url(r'^$', 'home', name="home_asistencia"),
-	url(r'^agregar$', 'agregar_view', name="agregar_asistencia"),
-	url(r'^editar/(?P<id>\d+)$', 'editar_view', name="editar_asistencia"),
-	url(r'^borrar/(?P<id>\d+)$', 'borrar_view', name="borrar_asistencia"),
-	url(r'^grafico$', 'home', name="grafico_asistencia"),
-	url(r'^listar$', 'listar', name="listar_asistencia"),
-	url(r'^listado$', 'listar', name="listado_asistencia"),
-	url(r'^ajax$', 'ajax', name="asistencia_ajax"),
-	
-	# url para ver un registro de asistencia
-	url(r'^(?P<pk>[\d]+)$', login_required(AsistenciaView.as_view()), name="asistencia_view_id"),
+    # home
+	url(r'^$', InicioView.as_view(), name="home_asistencia"),
 
-    url(r'^(\d+)$', 'manageID', name="asistencia.manageID"),
-    url(r'^(\d{4}-\d{2}-\d{2})$', 'manage', name="asistencia.manage"),
+	# add
+	url(r'^agregar$', login_required(CrearAsistenciaView.as_view()), name="agregar_asistencia"),
+	url(r'^add$', login_required(CrearAsistenciaView.as_view()), name="asistencia_add"),
+	url(r'^new$', login_required(CrearAsistenciaView.as_view()), name="asistencia_new"),
+
+	# edit
+	url(r'^(?P<pk>[\d]+)$', login_required(EditarAsistenciaView.as_view()), name="asistencia_view_id"),
+	url(r'^(?P<pk>[\d]+)/edit$', login_required(EditarAsistenciaView.as_view()), name="asistencia_edit"),
+
+	# delete
+	url(r'^(?P<pk>\d+)/borrar$', login_required(BorrarAsistenciaView.as_view()), name="borrar_asistencia"),
+	url(r'^(?P<pk>\d+)/delete$', login_required(BorrarAsistenciaView.as_view()), name="asistencia_delete"),
+
+	# list
+	url(r'^listar$', login_required(ListarAsistenciaView.as_view()), name="listar_asistencia"),
+	url(r'^listado$', login_required(ListarAsistenciaView.as_view()), name="listado_asistencia"),
+	url(r'^list$', login_required(ListarAsistenciaView.as_view()), name="asistencia_list"),
+
+	# graph
+	url(r'^grafico$', InicioView.as_view(), name="grafico_asistencia"),
+	url(r'^graph$', InicioView.as_view(), name="asistencia_graph"),
+
+	# ajax data
+	url(r'^ajax$', login_required(AjaxAsistenciaView.as_view()), name="asistencia_ajax"),
+	
 )
--- asistencia/views.py
+++ asistencia/views.py
@@ -4,9 +4,9 @@ from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from django.core import serializers
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse, reverse_lazy
 from django.utils import simplejson
-from django.views.generic.detail import DetailView
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView, ListView, TemplateView
 import json
 
 from asistencia.models import Asistencia
@@ -16,123 +16,60 @@ TEMPLATE_LISTADO = "asistencia/listado.html"
 TEMPLATE_GRAFICO = "asistencia/grafico.html"
 
 
-def render(request, template, dictionary=None):
-	return render_to_response(template, dictionary, context_instance=RequestContext(request))
-
-def home(request):
+class InicioView(TemplateView):
 	
-	if request.is_ajax():
-		
-		# list all asistencias
-		if request.method == "GET":
+	template_name = TEMPLATE_GRAFICO
 
-			return list(request)
-
-		# save a new asistencia
-		elif request.method == "POST":
-
-			return save(request)
-
-		else:
-
-			return render(request, TEMPLATE_GRAFICO)
-
-	else:
-		
-		return render(request, TEMPLATE_GRAFICO)
-
-def guardar(request):
-
-	if request.method == "POST":
-		
-		model = Asistencia()
-		data = request.POST
-
-		model.id = data["id"]
-		model.fecha = data["fecha"]
-		model.hermanos = data["hermanos"]
-		model.visitas = data["visitas"]
-		model.ninos = data["ninos"]
-		model.adolescentes = data["adolescentes"]
-		model.ofrenda = data["ofrenda"]
-		model.observaciones = data["observaciones"]	
-		
-		print model
-
-		model.save();
-		return True
-
-	return False
-
-@login_required
-def agregar_view(request):
-
-	if request.method == "POST":
-		
-		model = Asistencia()
-		data = request.POST
-
-		model.fecha = data["fecha"]
-		model.hermanos = data["hermanos"]
-		model.visitas = data["visitas"]
-		model.ninos = data["ninos"]
-		model.adolescentes = data["adolescentes"]
-		model.ofrenda = data["ofrenda"]
-		model.observaciones = data["observaciones"]	
+class CrearAsistenciaView(CreateView):
 	
-		model.save();
+	model = Asistencia
+	success_url = reverse_lazy('listar_asistencia')
+	template_name = TEMPLATE_ASISTENCIA
+
+class EditarAsistenciaView(UpdateView):
 	
-		return redirect("/asistencia")
+	context_object_name = "asistencia"
+	model = Asistencia
+	success_url = reverse_lazy('listar_asistencia')
+	template_name = TEMPLATE_ASISTENCIA
+
+	def get_context_data(self, **kwargs):
+		context = super(EditarAsistenciaView, self).get_context_data(**kwargs)
+		context['editar'] = True
+		return context
+
+
+class BorrarAsistenciaView(DeleteView):
 	
-	return render(request, TEMPLATE_ASISTENCIA)
+	model = Asistencia
+	success_url = reverse_lazy('listar_asistencia')
 
-@login_required
-def editar_view(request, id=0):
+	def get(self, request, *args, **kwargs):
+		return self.post(self, request, args, kwargs)
 
-	if request.method == "POST":
 
-		if guardar(request) == True:
-			return redirect("/asistencia/listar")
+class ListarAsistenciaView(ListView):
+	
+	context_object_name = "asistencias"
+	model = Asistencia
+	queryset = Asistencia.objects.order_by('-fecha')
+	template_name = TEMPLATE_LISTADO
 
-	elif request.method == "GET" and id != 0:
+class AjaxAsistenciaView(ListView):
+	
+	model = Asistencia
+	
+	def get(self, request, *args, **kwargs):
+		ini = 0
+		fin = 10
 
-		item = Asistencia.objects.get(id=id)
-		return render(request, TEMPLATE_ASISTENCIA, {
-			"asistencia": item,
-			"editar": True
-		})
+		if 'ini' in request.GET:
+			ini = int(request.GET["ini"])
 
-	else:
+		if 'fin' in request.GET:
+			fin = int(request.GET["fin"])
 
-		return redirect("/asistencia/agregar")
-
-@login_required
-def borrar_view(request, id):
-
-	model = Asistencia()
-	model.id = id
-	model.delete()
-	return redirect("/asistencia/listar")
-
-def listar(request):
-
-	page = 1
-	rows = 2048
-
-	if 'page' in request.GET:
-		page = int(request.GET['page'])
-
-	if 'rows' in request.GET:
-		rows = int(request.GET["rows"])
-
-	ini = ((page-1) * rows)
-	fin = ini + rows
-
-	#registros = Asistencia.objects.order_by('-fecha')[ini:fin]
-	registros = Asistencia.objects.order_by('-fecha')
-
-	if request.is_ajax():
-
+		registros = Asistencia.objects.order_by('-fecha')[ini:fin]
 		json_list = [ r.dict() for r in registros ]
 
 		for i in range(len(json_list)):
@@ -140,127 +77,4 @@ def listar(request):
 
 		return HttpResponse(json.dumps({"data": json_list}), content_type = 'application/json')
 
-	return render(request, TEMPLATE_LISTADO, {
-		"asistencias": registros
-	})
 
-def ajax(request):
-	
-	ini = 0
-	fin = 10
-
-	if 'ini' in request.GET:
-		ini = int(request.GET["ini"])
-
-	if 'fin' in request.GET:
-		fin = int(request.GET["fin"])
-
-	registros = Asistencia.objects.order_by('-fecha')[ini:fin]
-	json_list = [ r.dict() for r in registros ]
-
-	for i in range(len(json_list)):
-		json_list[i]['counter'] = ini + (i+1)
-
-	return HttpResponse(json.dumps({"data": json_list}), content_type = 'application/json')
-
-
-
-
-
-
-
-
-
-
-def manage(request, fecha):
-
-	if request.is_ajax():
-		
-		# select one asistencia
-		if request.method == "GET":
-
-			return select(request, fecha)
-
-		# delete one asistencia
-		elif request.method == "DELETE":
-
-			return delete(request, fecha)
-
-		else:
-
-			return render(request, "asistencia/grafico.html")
-
-
-	else:
-		
-		return render(request, "asistencia/grafico.html")
-
-def manageID(request, id):
-
-	# select one asistencia
-	if request.method == "GET":
-
-		return selectID(request, id)
-
-	if request.is_ajax():
-		
-		# update one asistencia
-		if request.method in ("PUT", "POST"):
-
-			return save(request)
-
-		# delete one asistencia
-		if request.method == "DELETE":
-
-			return deleteID(request, id)
-
-		else:
-
-			return render(request, "asistencia/grafico.html")
-
-	else:
-		
-		return render(request, "asistencia/grafico.html")
-
-def delete(request, fecha):
-	
-	model = asistencia()
-	model.fecha = fecha
-
-	model.delete()
-
-	return HttpResponse(simplejson.dumps(model.dict()), mimetype='application/json')
-
-def deleteID(request, id):
-	
-	model = asistencia()
-	model.id = str(id)
-
-	model.delete()
-
-	return HttpResponse(simplejson.dumps(model.dict()), mimetype='application/json')
-
-def select(request, fecha):
-	
-	model = Asistencia.objects.get(fecha=fecha)
-	return HttpResponse(simplejson.dumps(model.dict()), mimetype='application/json')
-
-def selectID(request, id):
-
-	model = Asistencia.objects.get(id=str(id))
-	return HttpResponse(simplejson.dumps(model.dict()), mimetype='application/json')
-
-def list(request):
-	
-	models = []
-	for model in Asistencia.objects.all().order_by("fecha"):
-		models.append(model.dict())
-
-	return HttpResponse(simplejson.dumps(models), mimetype='application/json')
-
-
-class AsistenciaView(DetailView):
-	model = Asistencia
-	context_object_name = "asistencia"
-	def get_template_names(self):
-		return TEMPLATE_ASISTENCIA
